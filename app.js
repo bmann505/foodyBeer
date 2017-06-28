10e57a0c7dbd2742d30c7f3e43ba22dc01e7502e
Brian Mann <bmann505@gamil.com>
2017-06-28 11:39:02 -0600

    less whitespace

--- app.js
+++ app.js
@@ -19,15 +19,18 @@ function matchBeers() {
       for (var i = 0; i < data.length; i++) {
         $('.beerDisplay').append(
           '<div class="col s12, m6, l4">' +
-          '<div class="card" style="overflow: scroll;">' +
+          '<div class="card small" style="overflow: scroll;">' +
           '<div class="card-image waves-effect waves-block waves-light">' +
-          '<img height="100px" width="50px" class="activator" src="' + data[i].image_url + '" style="width: 55px; margin-left: 1em; margin-top: .5em;" alt="beer label image">' +
+          '<img height="120px" width="60px" class="activator" src="' + data[i].image_url + '" style="width: 55px; margin-left: 1em; margin-top: .5em;" alt="beer label image">' +
+          '</div>' +
+          '<div class="title">' +
+          '<span class="card-title activator grey-text text-darken-4">' + data[i].name + '</span>' +
           '</div>' +
           '<div class="card-content">' +
-          '<span class="card-title activator grey-text text-darken-4">' + data[i].name + '<i class="material-icons right pairings">' + data[i].food_pairing[0] + '<br>' + data[i].food_pairing[1] + '<br>' + data[i].food_pairing[2] + '</i>' + '</span>' +
+          '<span class="activator grey-text text-darken-4">' + data[i].food_pairing[0] + '<br>' + data[i].food_pairing[1] + '<br>' + data[i].food_pairing[2] + '</span>' +
           '</div>' +
           '<div class="card-reveal">' +
-          '<span class="card-title grey-text text-darken-4">' + data[i].name + '<i class="material-icons right">' + 'close' + '</i>' + '</span>' +
+          '<span class="card-title grey-text text-darken-4">' + data[i].name + '<i class="material-icons right">' + 'X' + '</i>' + '</span>' +
           '<p>' + data[i].description + '</p>' +
           '</div>' +
           '</div>'
